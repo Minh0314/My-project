d5e3915ba9092cfe7c8bccf0ec0e3a651c8f0b0d
MacBook Min <macbookmin@MacBook-Pro.local>
2024-04-20 09:45:34 +0700

    update

--- Day45-b43/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import Login from "./Components/Login";
-import { useSelector, useDispatch } from "react-redux";
-import Dashboard from "./Components/Dashboard";
-export default function App() {
-  const isLogin = useSelector((state) => state.loginState.isLogin);
-  console.log(isLogin);
-  return <>{isLogin ? <Dashboard /> : <Login />}</>;
-}
